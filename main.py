83b06f81f23b26ee3d0ac379cfd7791fe191abe3
Mohan247 <discipline100times@gmail.com>
2025-07-14 12:21:10 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from keep_alive import keep_alive
+import os
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Client("mohanlpk_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@bot.on_message(filters.command("start"))
+async def start(client, message: Message):
+    await message.reply("👋 Hello! mohanlpk bot is alive!")
+
+keep_alive()  # Flask server for uptime
+bot.run()
